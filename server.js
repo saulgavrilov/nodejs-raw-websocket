8785d19939d290a67b1e7fe52bdc73429a84b7b2
saulgavrilov <shaulgav@gmail.com>
2022-09-07 18:07:14 +0300

    Receving messages from the client

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const { createServer } = require('http');
 const crypto = require('crypto');
 const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+const SEVEN_BITS_INTEGER_MARKER = 125;
+const SIXTEEN_BITS_INTEGER_MARKER = 126;
+const SIXTYFOUR_BITS_INTEGER_MARKER = 127;
+const FIRST_BIT = 128;
+const MASK_KEY_BYTES_LENGTH = 4;
 const PORT = 1337;
 
 const server = createServer((request, response) => {
@@ -11,9 +16,43 @@ server.on('upgrade', onSocketUpgrade);
 
 function onSocketUpgrade(req, socket, head) {
   const { 'sec-websocket-key': webClientSocketKey } = req.headers;
-  console.log(`${webClientSocketKey} connected`);
   const headers = prepareHandShakeHeaders(webClientSocketKey);
+
+  console.log(`${webClientSocketKey} connected`);
+
   socket.write(headers);
+  socket.on('readable', () => onSocketReadable(socket));
+}
+
+function onSocketReadable(socket) {
+  // consume optcode (first byte)
+  // 1 = 1 byte = 8 bits
+  socket.read(1);
+
+  const [markerAndPayloadLength] = socket.read(1);
+  // MASK Indicator
+  const lengthIndicatorInBits = markerAndPayloadLength - FIRST_BIT;
+
+  let messageLength = 0;
+
+  if (lengthIndicatorInBits <= SEVEN_BITS_INTEGER_MARKER) {
+    messageLength = lengthIndicatorInBits;
+  } else {
+    throw new Error(`Your message is too long we don't handle 64-bit message`);
+  }
+
+  const maskKey = socket.read(MASK_KEY_BYTES_LENGTH);
+  const encoded = socket.read(messageLength);
+  const decode = unmask(encoded, maskKey);
+  console.log(decode.toString());
+}
+
+function unmask(encodedBuffer, maskKey) {
+  let finalBuffer = Buffer.from(encodedBuffer);
+  for (let i = 0; i < encodedBuffer.length; i++) {
+    finalBuffer[i] = encodedBuffer[i] ^ maskKey[i % 4];
+  }
+  return finalBuffer;
 }
 
 function prepareHandShakeHeaders(id) {
